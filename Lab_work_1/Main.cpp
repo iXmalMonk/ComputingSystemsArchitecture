342b4b030c0a3375a67534fe10df3bee6ae6f7c0
iXmalMonk <ixmalmonk@gmail.com>
2023-11-26 02:59:24 +0300

    Update

--- Lab_work_1/Main.cpp
+++ Lab_work_1/Main.cpp
@@ -1,12 +1,83 @@
 #include <iostream>
 #include <omp.h>
 
-int main(int argc, char* argv[])
+void firstTask()
+{
+    int threadNum;
+    int numThread;
+    omp_set_num_threads(4);
+    std::cout << "Sequential area 1" << std::endl;
+#pragma omp parallel private(threadNum, numThread)
+    {
+        threadNum = omp_get_thread_num();
+        numThread = omp_get_num_threads();
+        std::cout << "Parallel region" << std::endl;
+    }
+    std::cout << "Sequential area 2" << std::endl;
+}
+
+void secondTask()
+{
+    int numThreads;
+    int threadNum;
+#pragma omp parallel
+    {
+        threadNum = omp_get_thread_num();
+#pragma omp master
+        {
+            numThreads = omp_get_num_threads();
+            std::cout << "Total number of threads: " << numThreads << std::endl;
+        }
+        std::cout << "Thread number: " << threadNum << std::endl;
+    }
+}
+
+void thirdTask()
+{
+#pragma omp parallel
+    {
+#pragma omp critical
+        {
+            std::cout << "Parallel region 1" << std::endl;
+        }
+    }
+#pragma omp parallel num_threads(3)
+    {
+#pragma omp critical
+        {
+            std::cout << "Parallel region 2" << std::endl;
+        }
+    }
+    omp_set_num_threads(4);
+#pragma omp parallel
+    {
+#pragma omp critical
+        {
+            std::cout << "Parallel region 3" << std::endl;
+        }
+    }
+}
+
+void fourthTask()
+{
+    int threadNum;
+    int numThread;
+    omp_set_num_threads(128);
+    omp_set_dynamic(0);
+#pragma omp parallel
+    {
+        threadNum = omp_get_thread_num();
+        numThread = omp_get_num_threads();
+        std::cout << threadNum << " / " << numThread << std::endl;
+    }
+}
+
+void fifthTask(int argc, char* argv[])
 {
     if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " parallel | serial" << std::endl;
-        return 1;
+        return;
     }
     std::string mode = argv[1];
     if (mode == "parallel")
@@ -23,7 +94,12 @@ int main(int argc, char* argv[])
     else
     {
         std::cout << "Invalid mode: " << mode << std::endl;
-        return 1;
+        return;
     }
+}
+
+int main(int argc, char* argv[])
+{
+    firstTask();
     return 0;
 }
